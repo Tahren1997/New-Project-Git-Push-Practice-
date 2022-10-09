bc5226f6e52f35bc38df6726538f81dc69b3dd9c
Tyler Chen <tchen22618@outlook.com>
2022-10-09 13:56:36 +0800

    2nd change

---
+++ Transition/js/main01.js
@@ -0,0 +1,12 @@
+//  Module
+
+
+function addTwo (a,b) {
+    console.log(a + b);
+}
+
+function minusTwo (a,b) {
+    console.log(a-b);
+}
+
+export {addTwo, minusTwo};
---
+++ Transition/js/main02.js
@@ -0,0 +1,6 @@
+import { addTwo, minusTwo } from "./main01.js";
+
+
+addTwo(1,2);
+
+minusTwo(4,1);
